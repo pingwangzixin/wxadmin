b1793c61f3a7ba65b73f4daa5dd224ec91016e56
wangxin <wangxin_8685@qq.com>
2020-06-30 15:50:17 +0800

    config

--- config/index.js
+++ config/index.js
@@ -36,8 +36,8 @@ module.exports = {
   build: {
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsSubDirectory: './static',
+    assetsPublicPath: './',
     productionSourceMap: true,
     devtool: '#source-map',
     productionGzip: false,
