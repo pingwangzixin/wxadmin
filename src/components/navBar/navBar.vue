39204cc0326da525e162203b24dc639e7cfa93ea
wangxin <wangxin_8685@qq.com>
2020-06-30 11:34:11 +0800

    navBar.vue

--- src/components/navBar/navBar.vue
+++ src/components/navBar/navBar.vue
@@ -16,7 +16,7 @@
 				          <span>wxAdmin</span>
 				        </template>
 				        <el-menu-item-group>
-				          <template slot="title">分组一</template>
+				          <template slot="title">表格</template>
 				          <el-menu-item index="1-1">选项1</el-menu-item>
 				          <el-menu-item index="1-2">选项2</el-menu-item>
 				        </el-menu-item-group>
