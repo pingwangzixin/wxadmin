63597dde5d83ce54440c7a457397f493129f7c77
wangxin <wangxin_8685@qq.com>
2020-06-30 14:57:40 +0800

    icon

--- src/components/navBar/navBar.vue
+++ src/components/navBar/navBar.vue
@@ -2,45 +2,23 @@
 	<div>
 		<div class="navBar">
 			<div>
-				<el-menu
-				      default-active="2"
-				      class="el-menu-vertical-demo"
-				      @open="handleOpen"
-				      @close="handleClose"
-				      background-color="#545c64"
-				      text-color="#fff"
-				      active-text-color="#ffd04b">
-				      <el-submenu index="1">
+				<!-- <el-menu router :default-active="activeIndex" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
+				    <el-submenu index="1">
 				        <template slot="title">
 				          <i class="el-icon-location"></i>
 				          <span>wxAdmin</span>
 				        </template>
 				        <el-menu-item-group>
-				          <template slot="title">表格</template>
-				          <el-menu-item index="1-1">选项1</el-menu-item>
-				          <el-menu-item index="1-2">选项2</el-menu-item>
+				          <el-menu-item index="/index">表格</el-menu-item>
+				          <el-menu-item index="/icon">icon</el-menu-item>
 				        </el-menu-item-group>
-				        <el-menu-item-group title="分组2">
-				          <el-menu-item index="1-3">选项3</el-menu-item>
-				        </el-menu-item-group>
-				        <el-submenu index="1-4">
-				          <template slot="title">选项4</template>
-				          <el-menu-item index="1-4-1">选项1</el-menu-item>
-				        </el-submenu>
-				      </el-submenu>
-				      <el-menu-item index="2">
-				        <i class="el-icon-menu"></i>
-				        <span slot="title">导航二</span>
-				      </el-menu-item>
-				      <el-menu-item index="3" disabled>
-				        <i class="el-icon-document"></i>
-				        <span slot="title">导航三</span>
-				      </el-menu-item>
-				      <el-menu-item index="4">
-				        <i class="el-icon-setting"></i>
-				        <span slot="title">导航四</span>
-				      </el-menu-item>
-				    </el-menu>
+				    </el-submenu>
+				</el-menu> -->
+				<el-menu :default-active="this.$router.path" router class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
+					 <el-menu-item v-for="(item,i) in navList" :key="i" :index="item.name">
+					  {{item.navItem}}
+					 </el-menu-item>
+				</el-menu>
 			</div>
 		</div>
 		<div class="contentRight">
@@ -55,7 +33,11 @@
 	export default {
 		data() {
 			return {
-				
+				navList:[
+					{name:'/index',navItem:'表格'},
+				    {name:'/icon',navItem:'icon'},
+				    {name:'/echarts',navItem:'echarts'},
+				]
 			}
 		},
 		methods:{
@@ -82,7 +64,7 @@
 		background:rgb(84, 92, 100);
 	}
 	ul>li{
-		text-align: left;
+		text-align: center;
 	}
 	.contentRight{
 		margin-left:210px;
--- src/config/env.js
+++ src/config/env.js
@@ -1,5 +1,5 @@
 // 配置编译环境和线上环境之间的切换
-let iconfontVersion = ['1862144_zrfhknx42kj'];
+let iconfontVersion = ['1862144_zrfhknx42kj','1712500_ifypg9050y'];
 let iconfontUrl = `//at.alicdn.com/t/font_$key.css`;
 
 export {
