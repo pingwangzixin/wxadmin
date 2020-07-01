f276e2ab94675c225437ee0354c0d7420d608c82
wangxin <wangxin_8685@qq.com>
2020-07-01 09:47:23 +0800

    config

--- config/index.js
+++ config/index.js
@@ -3,45 +3,45 @@
 // see http://vuejs-templates.github.io/webpack for documentation.
 
 const path = require('path');
-const baseUrl ='https://ysxs-patient.beta.microzan.com.cn';
+const baseUrl = 'https://ysxs-patient.beta.microzan.com.cn';
 
 module.exports = {
-  dev: {
+	dev: {
 
-    // Paths
-    assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
-    proxyTable: {
-		'/api':{  // api为匹配项
-		  target:baseUrl, // 设置代理目标
-		  changeOrigin: true,
-		  // pathRewrite: {  // 重写路径
-		  //   '^/api': ''
-		  // }
-		}
+		// Paths
+		assetsSubDirectory: 'static',
+		assetsPublicPath: '/',
+		proxyTable: {
+			'/api': { // api为匹配项
+				target: baseUrl, // 设置代理目标
+				changeOrigin: true,
+				// pathRewrite: {  // 重写路径
+				//   '^/api': ''
+				// }
+			}
+		},
+
+		// Various Dev Server settings
+		host: 'localhost', // can be overwritten by process.env.HOST
+		port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+		autoOpenBrowser: false,
+		errorOverlay: true,
+		notifyOnErrors: true,
+		poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
+		devtool: 'cheap-module-eval-source-map',
+		cacheBusting: true,
+		cssSourceMap: true
 	},
 
-    // Various Dev Server settings
-    host: 'localhost', // can be overwritten by process.env.HOST
-    port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
-    autoOpenBrowser: false,
-    errorOverlay: true,
-    notifyOnErrors: true,
-    poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
-    devtool: 'cheap-module-eval-source-map',
-    cacheBusting: true,
-    cssSourceMap: true
-  },
-
-  build: {
-    index: path.resolve(__dirname, '../dist/index.html'),
-    assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: './static',
-    assetsPublicPath: './',
-    productionSourceMap: true,
-    devtool: '#source-map',
-    productionGzip: false,
-    productionGzipExtensions: ['js', 'css'],
-    bundleAnalyzerReport: process.env.npm_config_report
-  }
+	build: {
+		index: path.resolve(__dirname, '../dist/index.html'),
+		assetsRoot: path.resolve(__dirname, '../dist'),
+		assetsSubDirectory: './static',
+		assetsPublicPath: './',
+		productionSourceMap: true,
+		devtool: '#source-map',
+		productionGzip: false,
+		productionGzipExtensions: ['js', 'css'],
+		bundleAnalyzerReport: process.env.npm_config_report
+	}
 }
