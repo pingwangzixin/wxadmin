aae8248cc21eb557163ca61fe611570bb9bb9f30
wangxin <wangxin_8685@qq.com>
2020-07-01 12:59:10 +0800

    vuex

--- src/components/vuex/getHtml.vue
+++ src/components/vuex/getHtml.vue
@@ -11,6 +11,13 @@
 				msg:"这是组件的信息",
 				info:'ref获取子组件信息'
 			}
+		},
+		methods:{
+			open(){
+				this.msg="子组件调用父组件方法";
+				this.$emit("refreshData")
+				console.log('1111')
+			}
 		}
 	}
 </script>
--- src/components/vuex/vuex.vue
+++ src/components/vuex/vuex.vue
@@ -3,7 +3,7 @@
 		<div style="margin:20px 0">vueX获取的数据：{{userId}}</div>
 		<p></p>
 		 <el-button type="primary" @click="xiugai">修改vuex数据</el-button>
-		 <div ref="getHtml"></div>
+		 <div ref="getHtml" @refreshData="getData"></div>
 		 <getHtml ref="getzujian"></getHtml>
 		 <el-button type="primary" @click="getData">获取子组件data中的值</el-button>
 		 <p ref="getInfo"></p>
@@ -27,8 +27,8 @@
 				this.$refs.getHtml.innerHTML = 'vue ref 操作dom,ref除了可以获取本页面的dom元素，还可以拿到子组件中的data和去调用子组件中的方法。注意：1、ref 需要在dom渲染完成后才会有，在使用的时候确保dom已经渲染完成。比如在生命周期 mounted(){} 钩子中调用，或者在 this.$nextTick(()=>{}) 中调用。2、如果ref 是循环出来的，有多个重名，那么ref的值会是一个数组 ，此时要拿到单个的ref 只需要循环就可以了。'
 			},
 			getData(){
-				this.$refs.getInfo.innerHTML=this.$refs.getzujian.info
-			}
+				this.$refs.getzujian.open()
+			},
 		},
 		components:{
 			getHtml
